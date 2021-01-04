37914b5e9433b7b4dc21b58599e85c1f9393d56e
DanieLucas28 <danielcerqueira@live.com>
2021-01-04 18:56:37 -0300

    Adicionar arquivos de projeto.

---
+++ Parametrizador/Splash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parametrizador
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 3;
+            if (panel2.Width >= 700)
+            {
+                timer1.Stop();
+                Home f2 = new Home();
+                f2.Show();
+                this.Hide();
+            }
+        }
+    }
+}
