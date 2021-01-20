e05f889b40207b5b7aaf2eaf6ef1e4e8c9b608b8
DanieLucas28 <danielcerqueira@live.com>
2021-01-20 14:58:11 -0300

    Adição de todas os forms para o tipo Cream Cracker
    forms adicionados:
    Mistura;
    Fermentação;
    Formação;
    Forneamento;
    Resfriamento;

---
+++ Parametrizador/Cream Cracker/FormMisturaCC.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Parametrizador.Cream_Cracker
+{
+    partial class FormMisturaCC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tempmassabox = new System.Windows.Forms.GroupBox();
+            this.linktempmassa = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tempmassa = new System.Windows.Forms.TextBox();
+            this.tempmassabox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(28, 519);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox2.Location = new System.Drawing.Point(28, 346);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox2.Size = new System.Drawing.Size(1134, 173);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tempo de mistura";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(28, 173);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(1134, 173);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fluxo de adição";
+            // 
+            // tempmassabox
+            // 
+            this.tempmassabox.Controls.Add(this.linktempmassa);
+            this.tempmassabox.Controls.Add(this.label1);
+            this.tempmassabox.Controls.Add(this.Tempmassa);
+            this.tempmassabox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tempmassabox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tempmassabox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempmassabox.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tempmassabox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tempmassabox.Location = new System.Drawing.Point(28, 0);
+            this.tempmassabox.Margin = new System.Windows.Forms.Padding(10);
+            this.tempmassabox.Name = "tempmassabox";
+            this.tempmassabox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tempmassabox.Size = new System.Drawing.Size(1134, 173);
+            this.tempmassabox.TabIndex = 4;
+            this.tempmassabox.TabStop = false;
+            this.tempmassabox.Text = "Temperatura da massa";
+            // 
+            // linktempmassa
+            // 
+            this.linktempmassa.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.linktempmassa.AutoEllipsis = true;
+            this.linktempmassa.BackColor = System.Drawing.Color.Transparent;
+            this.linktempmassa.DisabledLinkColor = System.Drawing.Color.DimGray;
+            this.linktempmassa.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.linktempmassa.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.linktempmassa.Location = new System.Drawing.Point(3, 91);
+            this.linktempmassa.Name = "linktempmassa";
+            this.linktempmassa.Size = new System.Drawing.Size(1128, 79);
+            this.linktempmassa.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Temperatura da massa nesse estágio";
+            // 
+            // Tempmassa
+            // 
+            this.Tempmassa.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Tempmassa.Location = new System.Drawing.Point(314, 44);
+            this.Tempmassa.Name = "Tempmassa";
+            this.Tempmassa.Size = new System.Drawing.Size(43, 27);
+            this.Tempmassa.TabIndex = 0;
+            this.Tempmassa.TextChanged += new System.EventHandler(this.Tempmassa_TextChanged);
+            // 
+            // FormMistura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(87)))), ((int)(((byte)(152)))));
+            this.ClientSize = new System.Drawing.Size(1162, 519);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tempmassabox);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMistura";
+            this.Text = "FormMistura";
+            this.tempmassabox.ResumeLayout(false);
+            this.tempmassabox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox tempmassabox;
+        private System.Windows.Forms.LinkLabel linktempmassa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tempmassa;
+    }
+}
---
+++ Parametrizador/Cream Cracker/FormMisturaCC.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parametrizador.Cream_Cracker
+{
+    public partial class FormMisturaCC : Form
+    {
+        public FormMisturaCC()
+        {
+            InitializeComponent();
+
+            //this.dataGridView1.Rows.Add("40 a 42", "40 a 42", "Máx. 36", "39 a 42") ;
+
+        }
+
+        private void Tempmassa_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(Tempmassa.Text.Trim(), out _))
+            {
+
+                if (Convert.ToInt32(Tempmassa.Text.Trim()) >= 40 && (Convert.ToInt32(Tempmassa.Text.Trim()) <= 42))
+                {
+                    this.linktempmassa.LinkArea = new LinkArea(0, 0);
+                    this.linktempmassa.UseMnemonic = false;
+                    this.linktempmassa.Text = "A temperatura indicada está dentro dos critérios estabelecidos por Manley, Davidson e Bertolino & Braga.";
+                    this.linktempmassa.Links.Add(67, 6, "#");
+                    this.linktempmassa.Links.Add(75, 8, "#");
+                    this.linktempmassa.Links.Add(86, 17, "#");
+                }
+                else if (Convert.ToInt32(Tempmassa.Text.Trim()) >= 42)
+                {
+                    this.linktempmassa.LinkArea = new LinkArea(0, 0);
+                    this.linktempmassa.UseMnemonic = false;
+                    this.linktempmassa.Text = "A temperatura está acima do previsto pelos autores. Quanto mais alta for a temperatura, mais macia e amolecida é a massa. Ou seja, altas temperaturas podem ocasionar modificações na estrutura da massa desejada e torná-la mais difícil de ser manuseada. Temperaturas elevadas podem ocasionar uma massa escaldada.";
+
+                }
+                else if (Convert.ToInt32(Tempmassa.Text.Trim()) >= 30 && (Convert.ToInt32(Tempmassa.Text.Trim()) <= 36))
+                {
+                    this.linktempmassa.LinkArea = new LinkArea(0, 0);
+                    this.linktempmassa.UseMnemonic = false;
+                    this.linktempmassa.Text = "Temperatura de acordo apenas com Moretto. Atenção para a estrutura da massa: laminados doces possuem massa caracterizada como macia, ou seja, uma massa com bastante ''liga''. Baixas temperaturas não favorecem a formação desse tipo de estrutura da massa. ";
+                    this.linktempmassa.Links.Add(33, 7, "#");
+                }
+                else if (Convert.ToInt32(Tempmassa.Text.Trim()) < 30)
+                {
+                    this.linktempmassa.LinkArea = new LinkArea(0,0);
+                    this.linktempmassa.UseMnemonic = false;
+                    this.linktempmassa.Text = "Baixa temperatura. Massa para laminados doces deve caracterizar-se como macia, ou seja, com bastante 'liga'. Baixas temperaturas não favorecem a formação desse tipo de estrutura da massa.";                  
+                }
+            }
+            else
+            {
+                this.linktempmassa.Text = "Digite um valor válido para a temperatura.";
+
+            }
+        }
+
+    }
+}
